4b5a3335c1abd3f5933d4512080ac745381c7fa2
JohnQuezada <johnaquezada@gmail.com>
2022-02-23 19:18:33 -0500

    UI basic class update

---
+++ src/com/careerdevs/StartGame.java
@@ -0,0 +1,14 @@
+package com.careerdevs;
+
+public class StartGame {
+    public static void main(String[] args) {
+
+
+        // Things to do
+
+        String[] playableWords = new String[]{
+                "guessing", "these", "words",
+                "is", "very", "easy"
+        };
+    }
+}
